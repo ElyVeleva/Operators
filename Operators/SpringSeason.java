03ac5bb9a90062b6f3d3c056581251d3ddd64d15
Elena Veleva <101432896+ElyVeleva@users.noreply.github.com>
2022-03-21 17:54:18 +0200

    Update SpringSeason.java

--- Operators/SpringSeason.java
+++ Operators/SpringSeason.java
@@ -6,9 +6,9 @@ public class SpringSeason {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Въведете месец: ");
-        int month = Integer.parseInt(scanner.nextLine());
+        byte month = Byte.parseByte(scanner.nextLine());
         System.out.print("Въведете ден: ");
-        int day = Integer.parseInt(scanner.nextLine());
+        byte day = Byte.parseByte(scanner.nextLine());
 
         System.out.println(month == 3 && day >= 20 && day <= 31 || month == 4 && day >= 1 && day <= 30 || month == 5 && day >= 1 && day <= 31 || day >= 1 && day <= 20 && month == 6 ? "Вярно" : "Грешно");
     }
